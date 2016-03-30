5e2631c8b55887cd631562b1804f2288717aa85f
lz84 <liuzhuo84@gmail.com>
2016-03-30 21:32:00 +0800

    删除没用的文件

--- bachelor_demo/demo/src/main/java/cn/com/bpsc/demo/devman/dao/IDevManDao.java
+++
@@ -1,8 +0,0 @@
-package org.bachelor.demo.devman.dao;
-
-import org.bachelor.demo.devman.domain.DevManDomain;
-import org.bachelor.dao.IGenericDao;
-
-public interface IDevManDao  extends IGenericDao<DevManDomain,String>{
-
-}
--- bachelor_demo/demo/src/main/java/cn/com/bpsc/demo/risklevel/dao/IPNRiskLevelDao.java
+++
@@ -1,9 +0,0 @@
-package org.bachelor.demo.risklevel.dao;
-
-import org.bachelor.demo.risklevel.domain.PNRiskLevel;
-import org.bachelor.dao.IGenericDao;
-
-
-public interface IPNRiskLevelDao extends IGenericDao<PNRiskLevel,String>{
-
-}
