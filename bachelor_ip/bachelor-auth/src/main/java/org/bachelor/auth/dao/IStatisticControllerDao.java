a4f9b76d3dcbba0f2bce793de8ab835d33c44b89
liuzhuo <liuzhuo84@hotmail.com>
2020-06-30 14:42:44 +0800

    删除1.0中附带的功能

--- bachelor_ip/bachelor-auth/src/main/java/org/bachelor/auth/dao/IStatisticControllerDao.java
+++
@@ -1,7 +0,0 @@
-package org.bachelor.auth.dao;
-import org.bachelor.auth.domain.StatisticController;
-import org.bachelor.dao.IGenericDao;
-
-public interface IStatisticControllerDao  extends IGenericDao<StatisticController, String>{
-
-}
