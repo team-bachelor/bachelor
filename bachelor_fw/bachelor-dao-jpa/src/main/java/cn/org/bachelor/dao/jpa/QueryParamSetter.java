5726312e63638b0689b7cf4009027f36101fba0c
sunjing <sunjing@pdmi.cn>
2020-07-14 16:53:18 +0800

    整理pom文件，更新版本,添加bachelor-dependencies版本控制工程

--- bachelor_fw/bachelor-dao-jpa/src/main/java/cn/org/bachelor/dao/jpa/QueryParamSetter.java
+++
@@ -1,7 +0,0 @@
-package cn.org.bachelor.dao.jpa;
-
-import org.hibernate.Query;
-
-public interface QueryParamSetter {
-	void set(Query query);
-}
--- bachelor_fw/bachelor-dao-mybatis/src/main/java/cn/org/bachelor/dao/QueryParamSetter.java
+++
@@ -1,7 +0,0 @@
-package cn.org.bachelor.dao;
-
-import org.hibernate.Query;
-
-public interface QueryParamSetter {
-	void set(Query query);
-}
