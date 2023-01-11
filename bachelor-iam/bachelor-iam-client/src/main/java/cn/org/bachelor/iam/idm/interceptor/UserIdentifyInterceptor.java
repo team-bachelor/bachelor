3828a023d2fd6178dbed108af9045ed327598a37
liuzhuo <liuzhuo84@hotmail.com>
2023-01-11 16:35:23 +0800

    增加areaName

--- bachelor-iam/bachelor-iam-client/src/main/java/cn/org/bachelor/iam/idm/interceptor/UserIdentifyInterceptor.java
+++ bachelor-iam/bachelor-iam-client/src/main/java/cn/org/bachelor/iam/idm/interceptor/UserIdentifyInterceptor.java
@@ -54,6 +54,7 @@ public class UserIdentifyInterceptor extends HandlerInterceptorAdapter {
         user.setAccessToken(request.getHeader(JwtToken.PayloadKey.ACCESS_TOKEN));
         user.setTenantId(request.getHeader(JwtToken.PayloadKey.TENANT_ID));
         user.setAreaId(request.getHeader(JwtToken.PayloadKey.AREA_ID));
+        user.setAreaName(request.getHeader(JwtToken.PayloadKey.AREA_NAME));
         Object o = request.getAttribute(ACCESS_BACKEND);
         if (o != null && "N".equals(o.toString())) {
             user.setAccessBackend(false);
