a0ea8e081b4d9e01e4806c7a5d52cdacf24d79b8
liuzhuo <liuzhuo84@hotmail.com>
2023-07-03 15:52:12 +0800

    登录验证码使用redis缓存

--- bachelor-iam/bachelor-idm/bachelor-idm-login/src/main/java/cn/org/bachelor/iam/idm/login/controller/LocalLoginController.java
+++ bachelor-iam/bachelor-idm/bachelor-idm-login/src/main/java/cn/org/bachelor/iam/idm/login/controller/LocalLoginController.java
@@ -30,10 +30,10 @@ public class LocalLoginController {
     @PostMapping("/login")
     public ResponseEntity login(HttpServletRequest request, @RequestBody UsernamePasswordCredential user, @RequestParam(value = "i", required = false) String index) {
         String captcha = (String) request.getSession().getAttribute("captcha");
-        log.debug("ver captcha:" + user.getCaptcha() + "=" + captcha);
-        index = StringUtils.isEmpty(index) ? user.getIndex() : index;
-        if (!CaptchaUtil.ver(user.getCaptcha(), request) &&
-                !RedisCacheHelper.verCaptcha(redisTemplate, index, captcha)) {
+        log.debug("ver captcha:" + user.getCaptcha() + ",session=" + captcha + ",cache=" + RedisCacheHelper.getCaptcha(redisTemplate, index));
+        index = StringUtils.isEmpty(user.getIndex()) ? index : user.getIndex();
+        if ( //!CaptchaUtil.ver(user.getCaptcha(), request) &&
+                !RedisCacheHelper.verCaptcha(redisTemplate, index, user.getCaptcha())) {
 //        if ( !RedisCacheHelper.verCaptcha(redisTemplate, index, captcha)) {
             throw new LoginException("请输入正确的验证码");
         }
