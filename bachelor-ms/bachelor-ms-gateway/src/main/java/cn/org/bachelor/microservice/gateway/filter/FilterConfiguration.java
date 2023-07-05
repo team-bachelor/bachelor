39c7e67623b4313c4a88cbce985ff5e71cfa5f8c
liuzhuo <liuzhuo84@hotmail.com>
2023-07-05 17:46:37 +0800

    解决delete和put存在的跨域问题

--- bachelor-ms/bachelor-ms-gateway/src/main/java/cn/org/bachelor/microservice/gateway/filter/FilterConfiguration.java
+++ bachelor-ms/bachelor-ms-gateway/src/main/java/cn/org/bachelor/microservice/gateway/filter/FilterConfiguration.java
@@ -1,23 +1,12 @@
 package cn.org.bachelor.microservice.gateway.filter;
 
-import org.apache.commons.lang.StringUtils;
-import cn.org.bachelor.iam.IamConstant;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.cors.reactive.CorsUtils;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import java.util.Collections;
 
 /**
  * @ClassName FilterConfiguration
@@ -28,40 +17,17 @@ import java.util.List;
  **/
 @Configuration
 public class FilterConfiguration {
-    private static final Logger logger = LoggerFactory.getLogger(FilterConfiguration.class);
 
-    // 这里为支持的请求头，如果有自定义的header字段请自己添加
-    private static final String ALLOWED_HEADERS = "x-requested-with, Content-Type, Authorization, credential, " +
-            "X-XSRF-TOKEN,token,username,client,SiteID," + IamConstant.HTTP_HEADER_TOKEN_KEY;
-    private static final String ALLOWED_METHODS = "*";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String ALLOWED_Expose = "*";
-    private static final String MAX_AGE = "18000L"; 
-    
     @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            logger.info("intercept request with uri : " + request.getURI());
-            if (CorsUtils.isCorsRequest(request)) {
-            	List<String> list = request.getHeaders().get("Origin");
-            	String origin = list.get(0);
-                ServerHttpResponse response = ctx.getResponse();
-                HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin",  StringUtils.isEmpty(origin) ? "*" : origin);
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-                // https://www.jianshu.com/p/f33e7f94dc06 火狐浏览器兼容性
-                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-                headers.add("Access-Control-Expose-Headers", ALLOWED_Expose);
-                headers.add("Access-Control-Allow-Credentials", "true");
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-                    response.setStatusCode(HttpStatus.OK);
-                    return Mono.empty();
-                }
-            }
-            return chain.filter(ctx);
-        };
+    CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
     }
-    
+
 }
