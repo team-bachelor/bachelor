c97b5295c99c2b2233b04817038e5e447b08704a
liuzhuo <liuzhuo84@hotmail.com>
2023-08-11 17:37:28 +0800

    规范化工程名称

--- bachelor_ip/bachelor-stat/src/main/java/org/bachelor/stat/dao/IStatAopDao.java
+++
@@ -1,9 +0,0 @@
-package org.bachelor.stat.dao;
-
-import org.bachelor.dao.IGenericDao;
-import org.bachelor.stat.domain.StatAop;
-
-public interface IStatAopDao extends IGenericDao<StatAop,String>{
-	
-	public void batchDelete(String delInfo);
-}
